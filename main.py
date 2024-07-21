e20610dd7b95737fd61796b39d23641d33db7111
murakaevtt <133036369+murakaevtt@users.noreply.github.com>
2024-07-21 23:46:34 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
+import keyboard
+
+VERSION = "alpha 0.0.1"
+
+
 print("\t\tWelcome to the Win Cleaner!")
+
+while kybrd != "q":
+    kybrd = keyboard.hook()
+
+    if kybrd == "Ctrl + v":
+        print(f"version: {VERSION}")
